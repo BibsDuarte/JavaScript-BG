0fae24f789f831c06fe45ec7db6eac63293661cf
BEATRIZ DUARTE DA COSTA PONTA <57055022819@imaccommand002.local>
2025-11-18 11:51:13 -0300

    Ëxercicio 02

--- exercicio02.js
+++ exercicio02.js
@@ -3,5 +3,38 @@
 // semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
 // 2 representa terça-feira e assim por diante.
 
-let numero = prompt("Informe um número entre 1 e 7")
-let numero = prompt("Digite o numero: ")
+let dia_semana = window.prompt("Informe um número entre 1 e 7")
+
+switch (dia_semana) {
+    case "1":
+        console.log ("Segunda-feira")
+        break;
+
+         case "2":
+        console.log ("Terça-feira")
+        break;
+
+        case "3":
+        console.log ("Quarta-feira")
+        break;
+
+        case "4":
+        console.log ("Quinta-feira")
+        break;
+
+        case "5":
+        console.log ("Sexta-feira")
+        break; 
+
+        case "6":
+        console.log ("Sábado")
+        break;
+
+        case "7":
+        console.log ("Domingo")
+        break;
+    default:
+        console.log ("O número informado não é valido")
+
+        break;
+}
